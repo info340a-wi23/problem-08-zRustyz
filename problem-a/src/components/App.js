cd8ba2b8a24b0883d35dcbb8e85586a95c8543ac
zRustyz <Russell.liu05@gmail.com>
2023-03-09 01:33:01 -0800

    finished

--- problem-a/src/components/App.js
+++ problem-a/src/components/App.js
@@ -5,6 +5,7 @@ import PetList from './PetList';
 import AboutPage from './AboutPage';
 import ResourcesPage from './ResourcesPage';
 import PetDetail from './PetDetail';
+import { Navigate, Routes, Route, Link } from 'react-router-dom';
 
 import SAMPLE_DOGS from '../data/dogs.json'; //a sample list of dogs (model)
 
@@ -13,29 +14,62 @@ function App(props) {
   const pets = SAMPLE_DOGS; //pretend this was loaded externally or via prop
 
   return (
-    <div>
-      <header className="jumbotron py-4 mb-4">
-        <div className="container">
-          <h1>Adopt a Pet</h1>
-        </div>
-      </header>
-    
-      <main className="container">
-        <div className="row">
-          <div className="col-3">
-            <AboutNav />
-          </div>
-          <div className="col">
-            <PetList pets={pets} />
-          </div>
-        </div>
-      </main>
-
-      <footer className="container">
-        <small>Images from <a href="http://www.seattlehumane.org/adoption/dogs">Seattle Humane Society</a></small>
-      </footer>
+  <div>
+  <header className="jumbotron py-4 mb-4">
+    <div className="container">
+      <h1><Link to="adopt">Adopt a Pet</Link></h1>
     </div>
+  </header>
+
+  <main className="container">
+    <div className="row">
+      <div className="col-3">
+        <AboutNav />
+      </div>
+      <div className="col">
+      <Routes>
+        <Route path="about" element={<AboutPage />} />
+        <Route path="resources" element={<ResourcesPage />} />
+        <Route path="adopt" element={<PetPage />} >
+          <Route path=":petName" element={<PetDetail />} />
+          <Route index element={<PetList pets={pets}/>} />
+        </Route>
+        <Route path='*' element={<Navigate to="adopt" />} />
+    </Routes>
+      </div>
+    </div>
+  </main>
+
+  <footer className="container">
+    <small>Images from <a href="http://www.seattlehumane.org/adoption/dogs">Seattle Humane Society</a></small>
+  </footer>
+</div>
+
+
   );
 }
 
 export default App;
+
+{/* <div>
+<header className="jumbotron py-4 mb-4">
+  <div className="container">
+    <h1>Adopt a Pet</h1>
+  </div>
+</header>
+
+<main className="container">
+  <div className="row">
+    <div className="col-3">
+      <AboutNav />
+    </div>
+    <div className="col">
+      <PetList pets={pets} />
+    </div>
+  </div>
+</main>
+
+<footer className="container">
+  <small>Images from <a href="http://www.seattlehumane.org/adoption/dogs">Seattle Humane Society</a></small>
+</footer>
+</div> */}
--- problem-a/src/components/PetDetail.js
+++ problem-a/src/components/PetDetail.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Button from 'react-bootstrap/Button';
 import Carousel from 'react-bootstrap/Carousel';
+import { useParams } from 'react-router-dom';
 
 import _ from 'lodash';
 
@@ -8,9 +9,9 @@ import SAMPLE_DOGS from '../data/dogs.json'; //a sample list of dogs (model)
 
 function PetDetail(props) {
 
-  const petNameString = ''; //REPLACE THIS WITH CORRECT VALUE
+  const petNameString = useParams().petName; //REPLACE THIS WITH CORRECT VALUE
 
-  //pretend we loaded external data    
+  //pretend we loaded external data
   let pet =  _.find(SAMPLE_DOGS, {name: petNameString}); //find pet in data
 
   if(!pet) return <h2>No pet specified</h2> //if unspecified
@@ -32,7 +33,7 @@ function PetDetail(props) {
         <Carousel indicators={false} prevLabel="" nextLabel="">
           {carouselItems}
         </Carousel>
-      </div>      
+      </div>
       <Button disabled variant="primary" size="lg">Adopt me now!</Button>
     </div>
   );
